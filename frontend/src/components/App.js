449a8749c3eb6bd6aa8ca7eb3b5fb5615cf07076
Yash sharma <71271069+Yashsharma1911@users.noreply.github.com>
2024-04-01 11:30:06 +0000

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -14,7 +14,7 @@ import Register from "../components/Register";
 import Settings from "../components/Settings";
 import { store } from "../store";
 import { push } from "react-router-redux";
-
+// new
 const mapStateToProps = (state) => {
   return {
     appLoaded: state.common.appLoaded,
